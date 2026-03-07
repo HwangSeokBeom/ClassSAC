dc1d68e555102f3c294d6a1d4b967fe4186bf95c
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-07 23:52:35 +0900

    [Feat]: 클래스 레파지토리 추상화

---
+++ ClassSAC/Domain/Course/Repository/CourseRepository.swift
@@ -0,0 +1,14 @@
+//
+//  CourseRepository.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/7/26.
+//
+
+import Foundation
+import RxSwift
+
+protocol CourseRepository: AnyObject {
+    func fetchCourses() -> Single<[Course]>
+    func toggleCourseLike(courseID: String, isLiked: Bool) -> Single<Void>
+}
