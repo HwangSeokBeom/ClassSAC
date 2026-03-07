79a8fe5168bd213ca45a4ac9f389ae5638d4c11e
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-08 00:09:48 +0900

    [Feat]: 클래스 DTO 코딩키 추가, remote 구현

---
+++ ClassSAC/Data/Course/DTO/CourseResponseDTO.swift
@@ -0,0 +1,52 @@
+//
+//  CourseListResponseDTO.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/6/26.
+//
+
+import Foundation
+
+struct CourseListResponseDTO: Decodable {
+    let data: [CourseDTO]
+}
+
+struct CourseDTO: Decodable {
+    let classID: String
+    let category: Int
+    let title: String
+    let description: String?
+    let price: Int?
+    let salePrice: Int?
+    let imageURL: String?
+    let imageURLs: [String]?
+    let createdAt: String
+    let isLiked: Bool
+    let creator: CourseCreatorDTO
+
+    enum CodingKeys: String, CodingKey {
+        case classID = "class_id"
+        case category
+        case title
+        case description
+        case price
+        case salePrice = "sale_price"
+        case imageURL = "image_url"
+        case imageURLs = "image_urls"
+        case createdAt = "created_at"
+        case isLiked = "is_liked"
+        case creator
+    }
+}
+
+struct CourseCreatorDTO: Decodable {
+    let userID: String
+    let nick: String
+    let profileImage: String?
+
+    enum CodingKeys: String, CodingKey {
+        case userID = "user_id"
+        case nick
+        case profileImage
+    }
+}
---
+++ ClassSAC/Data/Course/Remote/CourseRemoteDataSource.swift
@@ -0,0 +1,58 @@
+//
+//  CourseRemoteDataSource.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/7/26.
+//
+
+import Foundation
+import RxSwift
+
+final class CourseRemoteDataSource {
+
+    private let httpClient: ClassSACHTTPClienting
+
+    init(httpClient: ClassSACHTTPClienting) {
+        self.httpClient = httpClient
+    }
+
+    func fetchCourses() -> Single<CourseListResponseDTO> {
+        Single.create { [httpClient] single in
+            httpClient.request(
+                ClassSACAPI.courses(query: nil),
+                as: CourseListResponseDTO.self
+            ) { result in
+                switch result {
+                case .success(let responseDTO):
+                    single(.success(responseDTO))
+
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+
+            return Disposables.create()
+        }
+    }
+
+    func toggleCourseLike(
+        courseID: String,
+        isLiked: Bool
+    ) -> Single<Void> {
+        Single.create { [httpClient] single in
+            httpClient.requestVoid(
+                ClassSACAPI.likeCourse(courseId: courseID, likeStatus: isLiked)
+            ) { result in
+                switch result {
+                case .success:
+                    single(.success(()))
+
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+
+            return Disposables.create()
+        }
+    }
+}
