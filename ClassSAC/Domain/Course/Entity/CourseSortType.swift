41af041ccef896c6a5a13b3d5b6bfe19ac2a6f9d
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-07 23:50:51 +0900

    [Feat]: 클래스 조회 enum 정의

---
+++ ClassSAC/Domain/Course/Entity/CourseSortType.swift
@@ -0,0 +1,13 @@
+//
+//  CourseSortType.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/7/26.
+//
+
+import Foundation
+
+enum CourseSortType {
+    case latest
+    case originalPriceDescending
+}
