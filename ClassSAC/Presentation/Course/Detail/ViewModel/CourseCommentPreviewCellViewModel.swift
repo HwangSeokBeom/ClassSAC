57383ec71f095a97a9a1ab0d0656159a058c1665
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-09 04:29:17 +0900

    [Feat]: 상세 프리뷰셀뷰모델 구현

---
+++ ClassSAC/Presentation/Course/Detail/ViewModel/CourseCommentPreviewCellViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  CourseCommentPreviewCellViewModel.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/9/26.
+//
+
+import Foundation
+
+struct CourseCommentPreviewCellViewModel {
+    let commentID: String
+    let writerNick: String
+    let contentText: String
+    let createdAtText: String
+}
