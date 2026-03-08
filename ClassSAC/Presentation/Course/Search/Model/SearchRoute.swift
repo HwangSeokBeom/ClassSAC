704855d3d1b15485ec1dee6ad81dfcb185aaeda6
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-09 00:30:15 +0900

    [Feat]: 클래스 검색 모델 정의

---
+++ ClassSAC/Presentation/Course/Search/Model/SearchRoute.swift
@@ -0,0 +1,12 @@
+//
+//  SearchRoute.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/8/26.
+//
+
+import Foundation
+
+enum SearchRoute {
+    case courseDetail(courseID: String)
+}
---
+++ ClassSAC/Presentation/Course/Search/Model/SearchViewState.swift
@@ -0,0 +1,12 @@
+//
+//  SearchViewState.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/8/26.
+//
+
+struct SearchViewState {
+    let courses: [CourseListCellViewModel]
+    let isLoading: Bool
+    let emptyMessage: String?
+}
