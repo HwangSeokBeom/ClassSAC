f109044fe40e564f81b32e6f10b29423104524ec
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-09 04:41:44 +0900

    [Faet]: 댓글 리모트 구현

---
+++ ClassSAC/Data/Course/Remote/CourseCommentRemoteDataSource.swift
@@ -0,0 +1,41 @@
+//
+//  CourseCommentRemoteDataSource.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/9/26.
+//
+
+import Foundation
+import RxSwift
+
+final class CourseCommentRemoteDataSource {
+
+    private let httpClient: ClassSACHTTPClienting
+
+    init(httpClient: ClassSACHTTPClienting) {
+        self.httpClient = httpClient
+    }
+
+    func fetchComments(courseID: String) -> Single<CourseCommentListResponseDTO> {
+
+        Single.create { [httpClient] single in
+
+            httpClient.request(
+                ClassSACAPI.fetchCourseComments(courseId: courseID),
+                as: CourseCommentListResponseDTO.self
+            ) { result in
+
+                switch result {
+
+                case .success(let responseDTO):
+                    single(.success(responseDTO))
+
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
+
+            return Disposables.create()
+        }
+    }
+}
