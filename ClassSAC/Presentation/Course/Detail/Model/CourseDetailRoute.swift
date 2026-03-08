f2eaeca17a548fad53b72affb2c835100cd21d42
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-09 04:27:30 +0900

    [Feat]: 상세 라우트 구현

---
+++ ClassSAC/Presentation/Course/Detail/Model/CourseDetailRoute.swift
@@ -0,0 +1,12 @@
+//
+//  CourseDetailRoute.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/9/26.
+//
+
+import Foundation
+
+enum CourseDetailRoute {
+    case commentList(courseID: String)
+}
