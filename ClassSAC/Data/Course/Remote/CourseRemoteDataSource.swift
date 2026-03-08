4c6250ef9651f332de29b5d0bba4f5d0ca67dd0a
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-09 03:42:07 +0900

    [Feat]: 클래스 상세화면 리모트 추가

--- ClassSAC/Data/Course/Remote/CourseRemoteDataSource.swift
+++ ClassSAC/Data/Course/Remote/CourseRemoteDataSource.swift
@@ -17,7 +17,7 @@ final class CourseRemoteDataSource {
     }
 
     func fetchCourses() -> Single<CourseListResponseDTO> {
-        Single<CourseListResponseDTO>.create { [httpClient] single in
+        Single.create { [httpClient] single in
             httpClient.request(
                 ClassSACAPI.courses(query: nil),
                 as: CourseListResponseDTO.self
@@ -25,7 +25,24 @@ final class CourseRemoteDataSource {
                 switch result {
                 case .success(let responseDTO):
                     single(.success(responseDTO))
+                case .failure(let error):
+                    single(.failure(error))
+                }
+            }
 
+            return Disposables.create()
+        }
+    }
+
+    func fetchCourseDetail(courseID: String) -> Single<CourseDetailResponseDTO> {
+        Single.create { [httpClient] single in
+            httpClient.request(
+                ClassSACAPI.courseDetail(courseId: courseID),
+                as: CourseDetailResponseDTO.self
+            ) { result in
+                switch result {
+                case .success(let responseDTO):
+                    single(.success(responseDTO))
                 case .failure(let error):
                     single(.failure(error))
                 }
@@ -36,17 +53,14 @@ final class CourseRemoteDataSource {
     }
 
     func searchCourses(query: String) -> Single<CourseListResponseDTO> {
-        Single<CourseListResponseDTO>.create { [httpClient] single in
-            let searchCoursesQuery = SearchCoursesQuery(title: query)
-
+        Single.create { [httpClient] single in
             httpClient.request(
-                ClassSACAPI.searchCourses(query: searchCoursesQuery),
+                ClassSACAPI.searchCourses(query: SearchCoursesQuery(title: query)),
                 as: CourseListResponseDTO.self
             ) { result in
                 switch result {
                 case .success(let responseDTO):
                     single(.success(responseDTO))
-
                 case .failure(let error):
                     single(.failure(error))
                 }
@@ -60,14 +74,13 @@ final class CourseRemoteDataSource {
         courseID: String,
         isLiked: Bool
     ) -> Single<Void> {
-        Single<Void>.create { [httpClient] single in
+        Single.create { [httpClient] single in
             httpClient.requestVoid(
                 ClassSACAPI.likeCourse(courseId: courseID, likeStatus: isLiked)
             ) { result in
                 switch result {
                 case .success:
                     single(.success(()))
-
                 case .failure(let error):
                     single(.failure(error))
                 }
