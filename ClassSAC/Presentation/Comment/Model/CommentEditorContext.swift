ac7d625fc4784a4b69bebcf76d25de9472741ec2
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-10 00:19:33 +0900

    [Feaet]: CommentEditorContext 모델 구현

---
+++ ClassSAC/Presentation/Comment/Model/CommentEditorContext.swift
@@ -0,0 +1,15 @@
+//
+//  CommentEditorContext.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/9/26.
+//
+
+import Foundation
+
+struct CommentEditorContext {
+    let courseID: String
+    let courseTitle: String
+    let categoryTitle: String
+    let mode: CommentEditorMode
+}
