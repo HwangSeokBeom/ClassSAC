4adb01ed0108a9cfb6144209f69b8420c4bd5dc6
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-10 00:07:50 +0900

    [Feat]: 댓글화면 라우터 정의

---
+++ ClassSAC/Presentation/Comment/Model/CommentListRoute.swift
@@ -0,0 +1,12 @@
+//
+//  CommentListRoute.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/9/26.
+//
+
+import Foundation
+
+enum CommentListRoute {
+    case commentEditor(context: CommentEditorContext)
+}
