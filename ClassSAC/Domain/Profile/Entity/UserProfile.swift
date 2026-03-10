b304372a4c6b0390e98340729ccc29e629420022
HwangSeokBeom <tjrqja0714@gmail.com>
2026-03-10 20:47:18 +0900

    [Feat]: 프로필 엔티티 생성

---
+++ ClassSAC/Domain/Profile/Entity/UserProfile.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfile.swift
+//  ClassSAC
+//
+//  Created by Hwangseokbeom on 3/10/26.
+//
+
+import Foundation
+
+struct UserProfile {
+    let userID: String
+    let email: String
+    let nick: String
+    let profileImageURL: String?
+}
